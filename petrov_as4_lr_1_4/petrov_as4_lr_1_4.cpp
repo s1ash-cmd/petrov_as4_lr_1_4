892052070b817f4675edb7533b78a3595f075279
s1ash <s1ashpetrov@yandex.ru>
2023-09-28 15:51:42 +0300

    почти рабочий вариант

--- petrov_as4_lr_1_4/petrov_as4_lr_1_4.cpp
+++ petrov_as4_lr_1_4/petrov_as4_lr_1_4.cpp
@@ -18,20 +18,51 @@ struct station {
 	double eff;
 };
 
+void check_input_int(int& x) {
+	cin >> x;
+	while (cin.fail() or x < 0 or cin.peek() != '\n' or cin.peek() == ' ') {
+		cin.clear();
+		cin.ignore(10000, '\n');
+		cout << "incorrect input! try again ";
+		cin >> x;
+	}
+}
+
+void check_input_double(double& x) {
+	cin >> x;
+	while (cin.fail() or x < 0 or cin.peek() != '\n' or cin.peek() == ' ') {
+		cin.clear();
+		cin.ignore(10000, '\n');
+		cout << "incorrect input! try again ";
+		cin >> x;
+	}
+}
+
+void check_input_bool(bool& x) {
+	cin >> x;
+	while (cin.fail() or x < 0 or cin.peek() != '\n' || cin.peek() == ' ') {
+		cin.clear();
+		cin.ignore(10000, '\n');
+		cout << "incorrect input! try again ";
+		cin >> x;
+	}
+}
+
 pipe pipe_input() {
 	pipe new_pipe;
 
 	cout << "enter pipe name: ";
-	cin >> new_pipe.pname;
+	cin.ignore();
+	getline(cin, new_pipe.pname);
 
 	cout << "enter pipe length: ";
-	cin >> new_pipe.length;
+	check_input_double(new_pipe.length);
 
 	cout << "enter pipe diameter: ";
-	cin >> new_pipe.diameter;
+	check_input_double(new_pipe.diameter);
 
 	cout << "is the pipe under repair? (1 - yes, 0 - no): ";
-	cin >> new_pipe.repair;
+	check_input_bool(new_pipe.repair);
 
 	return new_pipe;
 }
@@ -40,22 +71,27 @@ station station_input() {
 	station new_station;
 
 	cout << "enter station name ";
-	cin >> new_station.sname;
+	cin.ignore();
+	getline(cin, new_station.sname);
 
 	cout << "enter the number of workshops ";
-	cin >> new_station.number_of_workshops;
+	check_input_int(new_station.number_of_workshops);
 
 	cout << "enter the number of workshops in work ";
-	cin >> new_station.workshops_in_work;
+	check_input_int(new_station.workshops_in_work);
 
 	while (new_station.number_of_workshops < new_station.workshops_in_work) {
 		cout << "the number of working workshops cant be more than the total number of workshops" << endl << "enter again ";
-		cin >> new_station.workshops_in_work;
+		check_input_int(new_station.workshops_in_work);
 	}
 
 	cout << "enter eff (0.0 - 1.0) ";
-	cin >> new_station.eff;
+	check_input_double(new_station.eff);
 
+	while (new_station.eff > 1) {
+		cout << "incorrect input! try again ";
+		check_input_double(new_station.eff);
+	}
 	return new_station;
 }
 
@@ -85,14 +121,14 @@ void station_output(const station& new_station) {
 	}
 };
 
-pipe pipe_change_status(pipe new_pipe) {
+pipe pipe_change_status(pipe new_pipe) { //????????????????????????????????????
 	new_pipe.repair = !new_pipe.repair;
 	cout << "pipe status has been changed" << endl;
 	cout << "new pipe repair status is: " << new_pipe.repair << endl;
 	return new_pipe;
 }
 
-station station_change_status(station new_station) {
+station station_change_status(station new_station) {//????????????????????????????????????????
 	cout << "the number of workshops: " << new_station.number_of_workshops << endl;
 	cout << "the number of working workshops: " << new_station.workshops_in_work << endl;
 	cout << "enter new number of working workshops: ";
@@ -105,7 +141,7 @@ station station_change_status(station new_station) {
 	return new_station;
 }
 
-void write_pipe_file(pipe new_pipe) {
+void write_pipe_file(pipe new_pipe) { //!!!
 	ofstream file_out;
 	file_out.open("file.txt", ofstream::app);
 	if (file_out.is_open()) {
@@ -124,7 +160,7 @@ void write_pipe_file(pipe new_pipe) {
 	}
 }
 
-void write_station_file(station new_station) {
+void write_station_file(station new_station) {//!!
 	ofstream file_out;
 	file_out.open("file.txt", ofstream::app);
 	if (file_out.is_open()) {
@@ -140,6 +176,50 @@ void write_station_file(station new_station) {
 	}
 	else {
 		cout << "couldnt open file" << endl;
+	}
+}
+
+void read_pipe_file(pipe& new_pipe) {
+	ifstream file_in;
+	string pstr;
+	file_in.open("file.txt");
+	if (file_in.is_open()) {
+		while (getline(file_in, pstr)) {
+			if (pstr == "pipe: ") {	//!!
+				cout << pstr << endl;
+				file_in >> new_pipe.pname;
+				file_in >> new_pipe.length;
+				file_in >> new_pipe.diameter;
+				file_in >> new_pipe.repair;
+				pipe_output(new_pipe);
+			}
+		}
+		file_in.close();
+	}
+	else {
+		cout << "file couldnt be open " << endl;
+	}
+}
+
+void read_station_file(station& new_station) {
+	ifstream file_in;
+	string sstr;
+	file_in.open("file.txt");
+	if (file_in.is_open()) {
+		while (getline(file_in, sstr)) {
+			if (sstr == "station: ") {
+				cout << sstr << endl;
+				file_in >> new_station.sname;
+				file_in >> new_station.number_of_workshops;
+				file_in >> new_station.workshops_in_work;
+				file_in >> new_station.eff;
+				station_output(new_station);
+			}
+		}
+		file_in.close();
+	}
+	else {
+		cout << "file couldnt be open " << endl;
 	}
 }
 
@@ -160,7 +240,8 @@ int main() {
 		cout << "0) exit" << endl;
 		cout << "===============================" << endl;
 		cout << "enter a number ";
-		cin >> choice;
+
+		check_input_int(choice);
 
 		switch (choice) {
 		case 1: {
@@ -171,7 +252,6 @@ int main() {
 			new_station = station_input();
 			break;
 		}
-
 		case 3: {
 			cout << "\npipe:" << endl;
 			pipe_output(new_pipe);
@@ -181,18 +261,39 @@ int main() {
 			break;
 		}
 		case 4: {
-			new_pipe = pipe_change_status(new_pipe);
+			if (!new_pipe.pname.empty()) {
+				new_pipe = pipe_change_status(new_pipe);
+			}
+			else {
+				cout << "no pipe found" << endl;
+			}
 			break;
 		}
 		case 5: {
-			new_station = station_change_status(new_station);
+			if (!new_station.sname.empty()) {
+				new_station = station_change_status(new_station);
+			}
+			else {
+				cout << "no station found" << endl;
+			}
 			break;
 		}
 		case 6: {
 			write_pipe_file(new_pipe);
 			write_station_file(new_station);
-
+			break;
 		}
+		case 7: {
+			read_pipe_file(new_pipe);
+			read_station_file(new_station);
+			break;
+		}
+		case 0: {
+			return 0;
+		}
+		default:
+			cout << "wrong choice. enter again " << endl;
+			break;
 		}
 	}
 }
